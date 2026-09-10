5f60453394873dcfc99e725f1b56bf4a76c10813
Meherab Hossen <mhnnishat0102@gmail.com>
2026-09-11 02:38:09 +0600

    upto background screen widget initial

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -10,6 +10,28 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text('Get Started With'),
+            TextFormField(),
+            TextFormField(),
+            FilledButton(
+              onPressed: () {},
+              child: Icon(Icons.arrow_circle_right_outlined),
+            ),
+            TextButton(onPressed: () {}, child: Text('Forgot Password?')),
+            RichText(
+              text: TextSpan(
+                text: "Don't have an account?",
+                style: TextStyle(color: Colors.black),
+                children: [TextSpan(text: 'Sign up')],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/ui/widgets/screen_background.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class ScreenBackground extends StatelessWidget {
+  const ScreenBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      
+    );
+  }
+}
