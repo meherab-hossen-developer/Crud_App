8a1d72c1b320adea9145d35b7276ef856d2a93eb
Meherab Hossen <mhnnishat0102@gmail.com>
2026-09-12 00:54:31 +0600

    textController

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -9,46 +9,51 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Get Started With',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(height: 24),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Email',
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Get Started With',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
                 ),
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Password'
+                const SizedBox(height: 24),
+                TextFormField(
+                  controller: _emailTEController,
+                  decoration: InputDecoration(hintText: 'Email'),
                 ),
-              ),
-              const SizedBox(height: 16),
-              FilledButton(
-                onPressed: () {},
-                child: Icon(Icons.arrow_circle_right_outlined),
-              ),
-              TextButton(onPressed: () {}, child: Text('Forgot Password?')),
-              RichText(
-                text: TextSpan(
-                  text: "Don't have an account?",
-                  style: TextStyle(color: Colors.black),
-                  children: [TextSpan(text: 'Sign up')],
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _passTEController,
+                  decoration: InputDecoration(hintText: 'Password'),
                 ),
-              ),
-            ],
+                const SizedBox(height: 16),
+                FilledButton(
+                  onPressed: () {},
+                  child: Icon(Icons.arrow_circle_right_outlined),
+                ),
+                TextButton(onPressed: () {}, child: Text('Forgot Password?')),
+                RichText(
+                  text: TextSpan(
+                    text: "Don't have an account?",
+                    style: TextStyle(color: Colors.black),
+                    children: [TextSpan(text: 'Sign up')],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
