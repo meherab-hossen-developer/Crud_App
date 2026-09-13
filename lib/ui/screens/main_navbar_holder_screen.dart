2ce9e0748ff10ebdd1cce49615a7adf20e8f656a
Meherab Hossen <mhnnishat0102@gmail.com>
2026-09-13 21:45:43 +0600

    account in nav

--- lib/ui/screens/main_navbar_holder_screen.dart
+++ lib/ui/screens/main_navbar_holder_screen.dart
@@ -1,3 +1,4 @@
+import 'package:crud_app/ui/screens/progress_task_screen.dart';
 import 'package:flutter/material.dart';
 
 class MainNavbarHolderScreen extends StatefulWidget {
@@ -10,10 +11,18 @@ class MainNavbarHolderScreen extends StatefulWidget {
 class _MainNavbarHolderScreenState extends State<MainNavbarHolderScreen> {
 
   int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    ProgressTaskScreen(),
+    ProgressTaskScreen(),
+    ProgressTaskScreen(),
+    ProgressTaskScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: TMAppBar(),
+      body: _screens[_selectedIndex],
       bottomNavigationBar: NavigationBar(
         selectedIndex: _selectedIndex,
         onDestinationSelected: (int index){
@@ -41,4 +50,39 @@ class _MainNavbarHolderScreenState extends State<MainNavbarHolderScreen> {
       )
     );
   }
+}
+
+class TMAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const TMAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.blue.withOpacity(0.6),
+      title: Row(
+        spacing: 10,
+        children: [
+          CircleAvatar(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Meherab Hossen',style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              )),
+              Text('mhnnishat0102@gmail.com',style: TextStyle(
+                fontSize: 10,
+                color: Colors.white,
+              )),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
